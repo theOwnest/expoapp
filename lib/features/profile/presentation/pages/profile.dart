7557a778faf5a4d318e4a85b8dfb56b6525d89dc
Asadbek <asadbekdeveloper@gmail.com>
2023-07-22 22:23:59 +0500

    Adding profile page

--- lib/features/profile/presentation/pages/profile.dart
+++ lib/features/profile/presentation/pages/profile.dart
@@ -1,3 +1,8 @@
+import 'package:expo_kg/shared/configs/texts.dart';
+import 'package:expo_kg/shared/constants/border_radius.dart';
+import 'package:expo_kg/shared/constants/colors.dart';
+import 'package:expo_kg/shared/constants/margin.dart';
+import 'package:expo_kg/shared/widgets/rounded_button.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -5,22 +10,49 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        SizedBox(
-          height: 60,
-          child: Text(
-            'Profile',
+    return Padding(
+      padding: marginHV10,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Ваш профиль',
+            style: h24,
           ),
-        ),
-        Expanded(
-          child: Center(
-            child: Text(
-              'Profile',
+          const SizedBox(
+            height: 10,
+          ),
+          const Text(
+            'Здесь находятся история покупок, статусы заказов, спасобы оплаты и другое.',
+            style: st14,
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Container(
+            padding: marginHV15,
+            decoration: BoxDecoration(
+              color: AppColor.lightGrey,
+              borderRadius: borderR10,
+            ),
+            child: const Column(
+              children: [
+                Text(
+                  'Войдите, чтобы покупать и продавать товары',
+                  style: h18,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                RoundedButton(
+                  title: 'Вход/регистрация',
+                  margin: EdgeInsets.zero,
+                ),
+              ],
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
