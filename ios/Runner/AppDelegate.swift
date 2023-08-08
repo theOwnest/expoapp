dc0c15c5c2174c67d5c9b97c91c952a03b54b49b
batir <zalgasbaevbatyrbek02@gmail.com>
2023-08-08 15:19:43 +0500

    Adapting to the ios environment

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import YandexMapsMobile
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    YMKMapKit.setApiKey("e6341052-a50a-4721-83da-b9e3f215e24c")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/features/chat/presentation/pages/messaging.dart
+++ lib/features/chat/presentation/pages/messaging.dart
@@ -3,6 +3,7 @@ import 'package:expo_kg/features/chat/presentation/cubit/chat_cubit.dart';
 import 'package:expo_kg/features/chat/presentation/widgets/chat_appbar.dart';
 import 'package:expo_kg/features/chat/presentation/widgets/chat_textfield.dart';
 import 'package:expo_kg/features/chat/presentation/widgets/message_elements.dart';
+import 'package:expo_kg/shared/constants/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:keyboard_dismisser/keyboard_dismisser.dart';
@@ -20,6 +21,7 @@ class MessagingPage extends StatelessWidget {
       create: (context) => ChatController(chat),
       child: const KeyboardDismisser(
         child: Scaffold(
+          backgroundColor: AppColor.white,
           body: SafeArea(
             child: Column(
               children: [
